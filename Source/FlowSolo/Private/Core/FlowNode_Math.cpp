161209e6431abfa00b22ec18eab079b5756dc2ed
MKazemAkhgary <l.van.beethoven9@gmail.com>
2025-11-03 19:46:57 +0330

    New Arithmetic/Logical operator nodes.
    Add following nodes for Int/Float
      +, -, *, /, >, >=, <, <=, ==, !=
    Add following nodes for Bool
      &&, ||, !, ^, If

---
+++ Source/FlowSolo/Private/Core/FlowNode_Math.cpp
@@ -0,0 +1,258 @@
+﻿// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
+#include "Core/FlowNode_Math.h"
+#include "FlowAsset.h"
+#include "FlowNodeMacros.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FlowNode_Math)
+
+UFlowNode_ArithmeticOperator::UFlowNode_ArithmeticOperator()
+{
+#if WITH_EDITOR
+	Category = TEXT("Math|Arithmetic");
+#endif
+	InputPins.Empty();
+	OutputPins.Empty();
+	AllowedSignalModes = { EFlowSignalMode::Enabled, EFlowSignalMode::Disabled };
+}
+UFlowNode_ComparisonOperator::UFlowNode_ComparisonOperator()
+{
+#if WITH_EDITOR
+	Category = TEXT("Math|Comparison");
+#endif
+	InputPins.Empty();
+	OutputPins.Empty();
+	AllowedSignalModes = { EFlowSignalMode::Enabled, EFlowSignalMode::Disabled };
+}
+UFlowNode_LogicalOperator::UFlowNode_LogicalOperator()
+{
+#if WITH_EDITOR
+	Category = TEXT("Math|Logic");
+#endif
+	InputPins.Empty();
+	OutputPins.Empty();
+	AllowedSignalModes = { EFlowSignalMode::Enabled, EFlowSignalMode::Disabled };
+}
+
+
+UFlowNode_ArithmeticOperatorFloat::UFlowNode_ArithmeticOperatorFloat()
+	: A(0.0f), B(0.0f), Out(0.0f)
+{
+}
+UFlowNode_ArithmeticOperatorInt::UFlowNode_ArithmeticOperatorInt()
+	: A(0), B(0), Out(0)
+{
+}
+
+
+UFlowNode_DivFloat::UFlowNode_DivFloat()
+{
+	B = 1.0f;
+}
+UFlowNode_DivInt::UFlowNode_DivInt()
+{
+	B = 1;
+}
+
+
+UFlowNode_ComparisonOperatorFloat::UFlowNode_ComparisonOperatorFloat()
+	: A(0.0f), B(0.0f), Out(false)
+{
+}
+UFlowNode_ComparisonOperatorInt::UFlowNode_ComparisonOperatorInt()
+	: A(0), B(0), Out(false)
+{
+}
+
+
+UFlowNode_LogicalOperator_Binary::UFlowNode_LogicalOperator_Binary() : A(false), B(false), Out(false)
+{
+}
+UFlowNode_Not::UFlowNode_Not() : A(false), Out(true)
+{
+}
+
+
+void UFlowNode_ArithmeticOperator::ExecuteInput(const FName& PinName)
+{
+	TriggerFirstOutput(true);
+}
+void UFlowNode_ComparisonOperator::ExecuteInput(const FName& PinName)
+{
+	TriggerFirstOutput(true);
+}
+void UFlowNode_LogicalOperator::ExecuteInput(const FName& PinName)
+{
+	TriggerFirstOutput(true);
+}
+
+
+void UFlowNode_ArithmeticOperatorFloat::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(A);
+	DECLARE_INPUT_PIN(B);
+	DECLARE_OUTPUT_PIN(Out);
+}
+void UFlowNode_ArithmeticOperatorInt::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(A);
+	DECLARE_INPUT_PIN(B);
+	DECLARE_OUTPUT_PIN(Out);
+}
+
+void UFlowNode_ComparisonOperatorFloat::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(A);
+	DECLARE_INPUT_PIN(B);
+	DECLARE_OUTPUT_PIN(Out);
+}
+void UFlowNode_ComparisonOperatorInt::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(A);
+	DECLARE_INPUT_PIN(B);
+	DECLARE_OUTPUT_PIN(Out);
+}
+void UFlowNode_LogicalOperator_Binary::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(A);
+	DECLARE_INPUT_PIN(B);
+	DECLARE_OUTPUT_PIN(Out);
+}
+void UFlowNode_Not::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(A);
+	DECLARE_OUTPUT_PIN(Out);
+}
+
+
+bool UFlowNode_ArithmeticOperatorFloat::EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr)
+{
+	// 1. Check if the pin being requested is our "Out" pin.
+	// 2. When this is called, the Flow framework has already populated A and B
+	//    by evaluating their input connections (if any).
+	// 3. We just call the virtual PerformOp to get the result.
+
+	if (OutputPinName == GET_MEMBER_NAME_CHECKED(UFlowNode_ArithmeticOperatorFloat, Out))
+	{
+		Out = PerformOp(A, B);
+
+		// Set the output pointers for the Flow framework
+		OutProperty = GetOutputProperty(OutputPinName);
+		OutDataPtr = OutProperty->ContainerPtrToValuePtr<void>(this);
+		return true;
+	}
+
+	return false;
+}
+
+bool UFlowNode_ArithmeticOperatorInt::EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr)
+{
+	if (OutputPinName == GET_MEMBER_NAME_CHECKED(UFlowNode_ArithmeticOperatorInt, Out))
+	{
+		Out = PerformOp(A, B);
+
+		OutProperty = GetOutputProperty(OutputPinName);
+		OutDataPtr = OutProperty->ContainerPtrToValuePtr<void>(this);
+		return true;
+	}
+
+	return false;
+}
+
+bool UFlowNode_ComparisonOperatorFloat::EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr)
+{
+	if (OutputPinName == GET_MEMBER_NAME_CHECKED(UFlowNode_ComparisonOperatorFloat, Out))
+	{
+		Out = PerformOp(A, B);
+
+		OutProperty = GetOutputProperty(OutputPinName);
+		OutDataPtr = OutProperty->ContainerPtrToValuePtr<void>(this);
+		return true;
+	}
+	return false;
+}
+
+bool UFlowNode_ComparisonOperatorInt::EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr)
+{
+	if (OutputPinName == GET_MEMBER_NAME_CHECKED(UFlowNode_ComparisonOperatorInt, Out))
+	{
+		Out = PerformOp(A, B);
+
+		OutProperty = GetOutputProperty(OutputPinName);
+		OutDataPtr = OutProperty->ContainerPtrToValuePtr<void>(this);
+		return true;
+	}
+	return false;
+}
+
+bool UFlowNode_LogicalOperator_Binary::EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr)
+{
+	if (OutputPinName == GET_MEMBER_NAME_CHECKED(UFlowNode_LogicalOperator_Binary, Out))
+	{
+		Out = PerformOp(A, B);
+
+		OutProperty = GetOutputProperty(OutputPinName);
+		OutDataPtr = OutProperty->ContainerPtrToValuePtr<void>(this);
+		return true;
+	}
+	return false;
+}
+
+bool UFlowNode_Not::EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr)
+{
+	if (OutputPinName == GET_MEMBER_NAME_CHECKED(UFlowNode_Not, Out))
+	{
+		Out = !A;
+
+		OutProperty = GetOutputProperty(OutputPinName);
+		OutDataPtr = OutProperty->ContainerPtrToValuePtr<void>(this);
+		return true;
+	}
+	return false;
+}
+
+
+UFlowNode_If::UFlowNode_If() : Condition(false)
+{
+#if WITH_EDITOR
+	Category = TEXT("Route|Logic");
+	NodeDisplayStyle = FlowNodeStyle::Logic;
+#endif
+	InputPins.Empty();
+	InputPins.Add(FFlowPin(TEXT("Execute")));
+
+	OutputPins.Empty();
+	OutputPins.Add(FFlowPin(TEXT("True")));
+	OutputPins.Add(FFlowPin(TEXT("False")));
+
+	AllowedSignalModes = { EFlowSignalMode::Enabled, EFlowSignalMode::Disabled };
+}
+
+void UFlowNode_If::ExecuteInput(const FName& PinName)
+{
+	if (PinName == TEXT("Execute"))
+	{
+		if (Condition)
+		{
+			TriggerOutput(TEXT("True"), true);
+		}
+		else
+		{
+			TriggerOutput(TEXT("False"), true);
+		}
+		return;
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Unknown pin!"))
+}
+
+void UFlowNode_If::CachePinProperties()
+{
+	Super::CachePinProperties();
+	DECLARE_INPUT_PIN(Condition);
+}
---
+++ Source/FlowSolo/Public/Core/FlowNode_Math.h
@@ -0,0 +1,448 @@
+﻿// Copyright https://github.com/MothCocoon/FlowGraph/graphs/contributors
+
+#pragma once
+
+#include "Nodes/FlowNode.h"
+#include "FlowNode_Math.generated.h"
+
+/**
+ * base class for all arithmetic operators.
+ */
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_ArithmeticOperator : public UFlowNode
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_ArithmeticOperator();
+
+	virtual void ExecuteInput(const FName& PinName) override;
+};
+
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_ArithmeticOperatorFloat : public UFlowNode_ArithmeticOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_ArithmeticOperatorFloat();
+
+	virtual void CachePinProperties() override;
+	virtual bool EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr) override;
+
+protected:
+	virtual float PerformOp(const float InA, const float InB) const PURE_VIRTUAL(UFlowNode_ArithmeticOperatorFloat::PerformOp, return 0.0f; );
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	float A;
+
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	float B;
+
+	UPROPERTY(SaveGame, meta = (FlowDataPin = "Output"))
+	float Out;
+};
+
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_ArithmeticOperatorInt : public UFlowNode_ArithmeticOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_ArithmeticOperatorInt();
+
+	virtual void CachePinProperties() override;
+	virtual bool EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr) override;
+
+protected:
+	virtual int32 PerformOp(const int32 InA, const int32 InB) const PURE_VIRTUAL(UFlowNode_ArithmeticOperatorInt::PerformOp, return 0; );
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	int32 A;
+
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	int32 B;
+
+	UPROPERTY(SaveGame, meta = (FlowDataPin = "Output"))
+	int32 Out;
+};
+
+
+
+//------------------------------------------------------------------------------
+// FLOAT ARITHMETIC
+//------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float + float"))
+class FLOWSOLO_API UFlowNode_AddFloat : public UFlowNode_ArithmeticOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual float PerformOp(const float InA, const float InB) const override { return InA + InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float - float"))
+class FLOWSOLO_API UFlowNode_SubFloat : public UFlowNode_ArithmeticOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual float PerformOp(const float InA, const float InB) const override { return InA - InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float * float"))
+class FLOWSOLO_API UFlowNode_MulFloat : public UFlowNode_ArithmeticOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual float PerformOp(const float InA, const float InB) const override { return InA * InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float / float"))
+class FLOWSOLO_API UFlowNode_DivFloat : public UFlowNode_ArithmeticOperatorFloat
+{
+	GENERATED_BODY()
+public:
+	UFlowNode_DivFloat();
+protected:
+	virtual float PerformOp(const float InA, const float InB) const override
+	{
+		return (InB != 0.0f) ? (InA / InB) : 0.0f;
+	}
+};
+
+
+
+//------------------------------------------------------------------------------
+// INT32 ARITHMETIC
+//------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 + int32"))
+class FLOWSOLO_API UFlowNode_AddInt : public UFlowNode_ArithmeticOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual int32 PerformOp(const int32 InA, const int32 InB) const override { return InA + InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 - int32"))
+class FLOWSOLO_API UFlowNode_SubInt : public UFlowNode_ArithmeticOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual int32 PerformOp(const int32 InA, const int32 InB) const override { return InA - InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 * int32"))
+class FLOWSOLO_API UFlowNode_MulInt : public UFlowNode_ArithmeticOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual int32 PerformOp(const int32 InA, const int32 InB) const override { return InA * InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 / int32"))
+class FLOWSOLO_API UFlowNode_DivInt : public UFlowNode_ArithmeticOperatorInt
+{
+	GENERATED_BODY()
+public:
+	UFlowNode_DivInt();
+protected:
+	virtual int32 PerformOp(const int32 InA, const int32 InB) const override
+	{
+		return (InB != 0) ? (InA / InB) : 0;
+	}
+};
+
+
+
+/**
+ * base class for all comparison operators.
+ */
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_ComparisonOperator : public UFlowNode
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_ComparisonOperator();
+
+	virtual void ExecuteInput(const FName& PinName) override;
+};
+
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_ComparisonOperatorFloat : public UFlowNode_ComparisonOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_ComparisonOperatorFloat();
+
+	virtual void CachePinProperties() override;
+	virtual bool EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr) override;
+
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const PURE_VIRTUAL(UFlowNode_ComparisonOperatorFloat::PerformComp, return false; );
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	float A;
+
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	float B;
+
+	UPROPERTY(SaveGame, meta = (FlowDataPin = "Output"))
+	bool Out;
+};
+
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_ComparisonOperatorInt : public UFlowNode_ComparisonOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_ComparisonOperatorInt();
+	virtual void CachePinProperties() override;
+	virtual bool EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr) override;
+
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const PURE_VIRTUAL(UFlowNode_ComparisonOperatorInt::PerformComp, return false; );
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	int32 A;
+
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	int32 B;
+
+	UPROPERTY(SaveGame, meta = (FlowDataPin = "Output"))
+	bool Out;
+};
+
+
+
+//------------------------------------------------------------------------------
+// FLOAT COMPARISONS
+//------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float < float"))
+class FLOWSOLO_API UFlowNode_LTFloat : public UFlowNode_ComparisonOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const override { return (InA < InB); }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float > float"))
+class FLOWSOLO_API UFlowNode_GTFloat : public UFlowNode_ComparisonOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const override { return (InA > InB); }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float <= float"))
+class FLOWSOLO_API UFlowNode_LEFloat : public UFlowNode_ComparisonOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const override { return (InA <= InB); }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float >= float"))
+class FLOWSOLO_API UFlowNode_GEFloat : public UFlowNode_ComparisonOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const override { return (InA >= InB); }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float == float"))
+class FLOWSOLO_API UFlowNode_EQFloat : public UFlowNode_ComparisonOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const override { return FMath::IsNearlyEqual(InA, InB); }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "float != float"))
+class FLOWSOLO_API UFlowNode_NEFloat : public UFlowNode_ComparisonOperatorFloat
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const float InA, const float InB) const override { return !FMath::IsNearlyEqual(InA, InB); }
+};
+
+
+
+//------------------------------------------------------------------------------
+// INT COMPARISONS
+//------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 < int32"))
+class FLOWSOLO_API UFlowNode_LTInt : public UFlowNode_ComparisonOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const override { return InA < InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 > int32"))
+class FLOWSOLO_API UFlowNode_GTInt : public UFlowNode_ComparisonOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const override { return InA > InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 <= int32"))
+class FLOWSOLO_API UFlowNode_LEInt : public UFlowNode_ComparisonOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const override { return InA <= InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 >= int32"))
+class FLOWSOLO_API UFlowNode_GEInt : public UFlowNode_ComparisonOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const override { return InA >= InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 == int32"))
+class FLOWSOLO_API UFlowNode_EQInt : public UFlowNode_ComparisonOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const override { return InA == InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "int32 != int32"))
+class FLOWSOLO_API UFlowNode_NEInt : public UFlowNode_ComparisonOperatorInt
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(const int32 InA, const int32 InB) const override { return InA != InB; }
+};
+
+
+
+/**
+ * base class for all logical operators.
+ */
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_LogicalOperator : public UFlowNode
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_LogicalOperator();
+
+	virtual void ExecuteInput(const FName& PinName) override;
+};
+
+//------------------------------------------------------------------------------
+// BOOL LOGIC
+//------------------------------------------------------------------------------
+
+/**
+ * Abstract base for 2-input logical operators (AND, OR, XOR).
+ */
+UCLASS(NotBlueprintable, Abstract)
+class FLOWSOLO_API UFlowNode_LogicalOperator_Binary : public UFlowNode_LogicalOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_LogicalOperator_Binary();
+
+	virtual void CachePinProperties() override;
+	virtual bool EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr) override;
+
+protected:
+	virtual bool PerformOp(bool InA, bool InB) const PURE_VIRTUAL(UFlowNode_LogicalOperator_Binary::PerformOp, return false; );
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	bool A;
+
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	bool B;
+
+	UPROPERTY(SaveGame, meta = (FlowDataPin = "Output"))
+	bool Out;
+};
+
+
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "bool AND"))
+class FLOWSOLO_API UFlowNode_And : public UFlowNode_LogicalOperator_Binary
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(bool InA, bool InB) const override { return InA && InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "bool OR"))
+class FLOWSOLO_API UFlowNode_Or : public UFlowNode_LogicalOperator_Binary
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(bool InA, bool InB) const override { return InA || InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "bool XOR"))
+class FLOWSOLO_API UFlowNode_Xor : public UFlowNode_LogicalOperator_Binary
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(bool InA, bool InB) const override { return InA != InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "bool XNOR"))
+class FLOWSOLO_API UFlowNode_EQBool : public UFlowNode_LogicalOperator_Binary
+{
+	GENERATED_BODY()
+protected:
+	virtual bool PerformOp(bool InA, bool InB) const override { return InA == InB; }
+};
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "NOT"))
+class FLOWSOLO_API UFlowNode_Not : public UFlowNode_LogicalOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_Not();
+
+	virtual void CachePinProperties() override;
+	virtual bool EvaluateAndGetOutputValue(const FName OutputPinName, FProperty*& OutProperty, const void*& OutDataPtr) override;
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	bool A;
+
+	UPROPERTY(SaveGame, meta = (FlowDataPin = "Output"))
+	bool Out;
+};
+
+
+
+UCLASS(NotBlueprintable, meta = (DisplayName = "If"))
+class FLOWSOLO_API UFlowNode_If : public UFlowNode_LogicalOperator
+{
+	GENERATED_BODY()
+
+public:
+	UFlowNode_If();
+
+	virtual void ExecuteInput(const FName& PinName) override;
+	virtual void CachePinProperties() override;
+
+public:
+	UPROPERTY(EditAnywhere, SaveGame, meta = (FlowDataPin = "Input"))
+	bool Condition;
+};
